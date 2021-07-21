b6c3742c4b4b74ec7e1a1f85ebe4875c4090102f
Sofia Tejada <sofianais@icloud.com>
2021-07-21 13:56:32 -0700

    not sure what i'm doing, or where the heck acc needs to go or not go and...yeah

--- reduce.js
+++ reduce.js
@@ -1,3 +1,19 @@
 export const reduce = (arr, callback, initialValue) => {
-  
+//   let acc = initialValue === undefined ? 0 : initialValue;
+//   for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     if(element) {
+//       acc = callback(acc, element);
+//     }
+    
+  //   }
+  const acc = initialValue;
+
+  for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+      
+  }
+
+
+  return acc;
 };
--- reduce.test.js
+++ reduce.test.js
@@ -2,9 +2,9 @@ import { reduce } from './reduce';
 
 describe('reduce', () => {
   it('does lots of stuff i guess, i don\'t get it yet', () => {
-    const originalArray = [];
+    const originalArray = [2, 18, 25, 1, 36, 217, 3, 21, 24];
     const callback = (acc, item) => {};
-    const actual = reduce(originalArray, callback, initialValue);
+    const actual = reduce(originalArray, callback, 0);
     const expected = [];
 
     expect(actual).toEqual(expected);
