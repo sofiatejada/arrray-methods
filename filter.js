e3e74985a080bf72e566d737d7db7e0c6545db25
Sofia Tejada <sofianais@icloud.com>
2021-07-21 12:08:39 -0700

    it was a simple fix, thankfully! took ages to get to, tho

--- filter.js
+++ filter.js
@@ -2,9 +2,10 @@ export const filter = (arr, callback) => {
   let newArr = [];
 
   for (let index = 0; index < arr.length; index++) {
-    const item = callback(index);
-    if(item === true) {
-      newArr = [...newArr, item];
+    const item = callback(arr[index]);
+    if(item) {
+      console.log(arr[index]);
+      newArr = [...newArr, arr[index]];
     }
   }
   return newArr;
