c2b9d7d0dbb856d5a913d94d2a7bd29e64de3bcf
Sofia Tejada <sofianais@icloud.com>
2021-07-21 10:34:42 -0700

    start writing out a function, still don't know quite how to do it yet

---
+++ map.js
@@ -0,0 +1,9 @@
+export const map = (arr, callback) => {
+  // for(index value; condition to test when the loop should end; and a way to increment the index value)
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+      
+  }
+};
+
+
--- map.test.js
+++ map.test.js
@@ -1,7 +1,9 @@
+import { map } from "./map";
+
 describe('map', () => {
   it('takes an array and invokes the passed in callback function for each element in passed in array', () => {
     const originalArray = [1, 2, 3, 4, 5];
     const callback = (item) => item * 2;
     const result = map(originalArray, callback);
-  })
-})
+  });
+});
