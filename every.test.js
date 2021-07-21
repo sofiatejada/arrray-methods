25f01af7ede9b26942740c64c4dd71660051a3e2
Sofia Tejada <sofianais@icloud.com>
2021-07-21 14:01:14 -0700

    the test is written

--- every.test.js
+++ every.test.js
@@ -4,6 +4,9 @@ describe('every', () => {
   it('returns an overall true value if all callback return true or a truthy value', () => {
     const originalArray = [2, 18, 25, 1, 36, 217, 3, 21, 24];
     const callback = (item) => (item % 2 === 0);
-    const actual = every
+    const actual = every(originalArray, callback);
+    const expected = false;
+
+    expect(actual).toEqual(expected);
   });
 });
