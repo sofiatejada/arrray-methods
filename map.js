8d2197f35f5327cf68f5f061daf3202fc0efa448
Sofia Tejada <sofianais@icloud.com>
2021-07-21 11:17:42 -0700

    just a bit of cleanup

--- map.js
+++ map.js
@@ -4,14 +4,13 @@ export const map = (arr, callback) => {
 
   for (let index = 0; index < arr.length; index++) {
     const item = arr[index];
+    
     //THIS DOWN HERE DOES THE SAME THING AS PUSH
     // reason why this is the same as push:
     // when the loop goes around, as long as index is less than the length of the array,
     // newArr at [index], will be set to the new evaluation of the next callback(item)
     newArr[index] = callback(item);
-    //THOSE TWO DO THE SAME THING, PICK ONE
-    // newArr.push(item);
-      
+    // newArr.push(item); 
   }
   return newArr;
 };
