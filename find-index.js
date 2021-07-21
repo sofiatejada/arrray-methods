a29035b9b4142332b2c4b1b79a86323507c1cc7f
Sofia Tejada <sofianais@icloud.com>
2021-07-21 12:22:17 -0700

    i had it right the first time, just needed to return it...what a buffoon

--- find-index.js
+++ find-index.js
@@ -1,11 +1,11 @@
 export const findIndex = (arr, callback) => {
-  const newArr = [];
+  let newArr = [];
 
   for (let index = 0; index < arr.length; index++) {
     const item = callback(arr[index]);
     console.log(item);
     if(item) {
-      newArr[index] = index;
+      newArr = [...newArr, index];
     }
   }
   return newArr;
