3f12e132be2318c413694edea8dd462404ec35bb
Sofia Tejada <sofianais@icloud.com>
2021-07-21 10:52:57 -0700

    playing around with it because dan's explaining a lot

--- map.js
+++ map.js
@@ -1,9 +1,16 @@
 export const map = (arr, callback) => {
   // for(index value; condition to test when the loop should end; and a way to increment the index value)
-  for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+  const newArr = [];
+
+  for (let index = 0; index < arr.length; index++) {
+    const item = arr[index];
+    //THIS DOWN HERE DOES THE SAME THING AS PUSH
+    newArr[index] = callback(item);
+    newArr[index] = item;
+    // newArr.push(item);
       
   }
+  return newArr;
 };
 
 
