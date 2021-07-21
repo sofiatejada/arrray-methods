611a819f13305840bea298806208afaef19c8fe7
Sofia Tejada <sofianais@icloud.com>
2021-07-21 10:12:33 -0700

    started writing test

---
+++ map.test.js
@@ -0,0 +1,7 @@
+describe('map', () => {
+  it('takes an array and invokes the passed in callback function for each element in passed in array', () => {
+    const originalArray = [1, 2, 3, 4, 5];
+    const callback = (item) => item * 2;
+    const result = map(originalArray, callback);
+  })
+})
