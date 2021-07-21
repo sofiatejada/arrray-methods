907c94a7788a8ed1c910f8961ae319877a543850
Sofia Tejada <sofianais@icloud.com>
2021-07-21 11:26:32 -0700

    wrote out the test, hope it's what we're supposed to do...it fails, now to write the function

--- filter.test.js
+++ filter.test.js
@@ -1,5 +1,12 @@
+import { filter } from './filter';
+
 describe('filter', () => {
   it('creates a new array with all items whose callback returned true or a truthy value', () => {
-    
+    const originalArray = [2, 18, 25, 1, 36, 217, 3, 21, 24];
+    const callback = (item) => item % 2 === 0;
+    const actual = filter(originalArray, callback);
+    const expected = [2, 18, 36, 24];
+
+    expect(actual).toEqual(expected);
   });
 });
