57bdfd70cfab1506b88afb824bc9811779b0f80a
Sofia Tejada <sofianais@icloud.com>
2021-07-21 11:29:06 -0700

    so it returns true or false and stuff, but that's not what i want

--- filter.js
+++ filter.js
@@ -1,4 +1,12 @@
 export const filter = (arr, callback) => {
-  
+  const newArr = [];
+
+  for (let index = 0; index < arr.length; index++) {
+    const item = arr[index];
+    
+    newArr[index] = callback(item);
+      
+  }
+  return newArr;
 };
 
