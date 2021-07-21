e1d1744ea043b709db8db8c036324b4ab1d99855
Sofia Tejada <sofianais@icloud.com>
2021-07-21 12:12:56 -0700

    just getting started on the next one

--- filter.js
+++ filter.js
@@ -4,7 +4,6 @@ export const filter = (arr, callback) => {
   for (let index = 0; index < arr.length; index++) {
     const item = callback(arr[index]);
     if(item) {
-      console.log(arr[index]);
       newArr = [...newArr, arr[index]];
     }
   }
---
+++ find-index.test.js
@@ -0,0 +1,7 @@
+describe('findIndex', () => {
+  it('returns the index of the first item whose callback returns true or a truthy value', () => {
+    const originalArray = [2, 18, 25, 1, 36, 217, 3, 21, 24];
+    const callback = (item) => (item % 2 === 0);
+    const actual = 
+  });
+});
