c0ce30ecd747af57aaaafe40b303d3e45677fcd0
Sofia Tejada <sofianais@icloud.com>
2021-07-21 12:20:22 -0700

    it doesn't work, but it's a start

--- find-index.js
+++ find-index.js
@@ -1,3 +1,11 @@
 export const findIndex = (arr, callback) => {
-  
+  const newArr = [];
+
+  for (let index = 0; index < arr.length; index++) {
+    const item = callback(arr[index]);
+    console.log(item);
+    if(item) {
+      newArr[index] = index;
+    }
+  }
 };
